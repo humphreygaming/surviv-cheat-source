58c952b63e7e8d42efbd10fa538b244515cb34b0
Finn Lancaster <flancast90@gmail.com>
2021-10-01 19:14:01 -0400

    Delete src/cheat directory

--- src/cheat/random.js
+++
@@ -1,8 +0,0 @@
-module.exports = () => {
-    return btoa(
-        (
-            (new Date().getDate() + new Date().getMonth()) *
-            Math.pow(new Date().getFullYear(), 2)
-        ).toString(32)
-    ).replace(/[^a-zA-Z]/g, "")
-}
