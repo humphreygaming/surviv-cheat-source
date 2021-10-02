e00d3a801e8fb605d6eea9d1ff937482ff6e16d9
HumphreyGaming <39714670+humphreygaming@users.noreply.github.com>
2021-10-02 14:21:00 -0700

    Bump version, requires self compile for correct secret

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ try {
     fs.writeFileSync("./src/cheat/data.json", "{}")
     manifest = {}
 }
-var version = (manifest.version = "2.1.7")
+var version = (manifest.version = "2.1.8")
 
 var time = new Date()
 
